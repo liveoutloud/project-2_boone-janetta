59bde5bd5f1acb1132c23495a117b88760ec6e65
Janetta Boone <j.boone@ufl.edu>
2018-08-04 11:45:34 -0500

    form styled

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 //Write 5 lines of custom javascript in here
+
+//Form button validation
+$(function(){
+  $('.input').keyup(function() {
+
+    var empty = false;
+    $('.input').each(function(){
+      if ($(this).val() == '') {
+        empty = true;
+      }
+    });
+
+    if (empty) {
+      $('#submit').attr('disabled', 'disabled').hasClass('.button--disabled').removeClass('button--primary');
+    } else {
+      $('#submit').removeAttr('disabled').addClass('button--primary').removeClass('button--disabled');
+    }
+  });
+})()
--- js/modal.js
+++ js/modal.js
@@ -7,19 +7,12 @@ var btn = document.getElementById("myBtn");
 //Gets the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-//Opens modal onclick
+//Open
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-//Closes the modal onclick the (x)
+//Close (x)
 span.onclick = function() {
     modal.style.display = "none";
-}
-
-//Close modal when anywhere outside is clicked
-window.onlick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
 }
