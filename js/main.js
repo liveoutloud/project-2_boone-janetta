955b1d3883fc2462a96354bb4985b909fa8d50bb
Janetta Boone <j.boone@ufl.edu>
2018-08-07 21:25:55 -0500

    button mixin, made modal responsive, fixed select box

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-//5 lines of javascript
-
-//Form button validation
 $(function(){
   $('.input').keyup(function() {
 
@@ -19,14 +16,12 @@ $(function(){
   });
 })
 
-//More custom Javascript: This event listener adds a box to the h3 to highlight the Call To Action when it is moused-over.
 var hThree = document.getElementById( 'hThree' );
 
 hThree.addEventListener( 'mouseover', function() {
   hThree.className = 'box';
 })
 
-//Custom jquery: ready function calls all h4's and adds a border to the bottom of each h4 article headline.
 $(document).ready(function(){
     var h4 = $("h4");
     h4.css({
--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,13 @@
-//Gets the modal
 var modal = document.getElementById("myModal");
 
-//Opens the modal
 var btn = document.getElementById("myBtn");
 
-//Gets the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-//Open
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-//Close (x)
 span.onclick = function() {
     modal.style.display = "none";
 }
